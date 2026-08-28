71623cc50055102169555c6c92791b260bd10299
Davi MB <bissondavi@gmail.com>
2026-08-28 08:51:10 -0300

    Add Hiena, LoserClub, and Polemom classes

---
+++ classes.cs
@@ -0,0 +1,20 @@
+public class Hiena 
+{
+    private string? Especie;
+    private bool? Ri;
+    private string? Regiao;
+}
+
+public class LoserClub 
+{
+    private string? Nome;
+    private int? Idade;
+    private bool? Vivo;
+}
+
+public class Polemom 
+{
+    private string? Nome;
+    private string? Tipo;
+    private int? ;
+}
