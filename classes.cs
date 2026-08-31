fad03150c6219013769e3668e9ee5e6a19de51c6
Davi MB <bissondavi@gmail.com>
2026-08-31 13:51:50 -0300

    Refactor properties in Hiena, LoserClub, and Pokemon classes
    Refactor class properties to follow C# naming conventions and add new properties.

--- classes.cs
+++ classes.cs
@@ -1,20 +1,82 @@
 public class Hiena 
 {
-    private string? Especie;
-    private bool? Ri;
-    private string? Regiao;
+    private string? especie;
+    private bool? ri;
+    private string? regiao;
+    public string? Especie
+    {
+        get { return especie; }
+        set { especie = value; }
+    }
+    public bool? Ri;
+    public string? Regiao
+    {
+        get { return regiao; }
+        set { regiao = value; }
+    }
+    public Hiena(string especie, bool ri, string regiao)
+    {
+        Especie = especie;
+        Ri = ri;
+        Regiao = regiao;
+    }
+ 
+    public void exibirinfo()
+    {
+        Console.Write($"Hiena");
+        Console.WriteLine($"Espécie: {especie}");
+        Console.WriteLine($"Ri: {ri}");
+        Console.WriteLine($"Regiao: {regiao}");
+    }
 }
 
 public class LoserClub 
 {
-    private string? Nome;
-    private int? Idade;
-    private bool? Vivo;
+    private string? nome;
+    private int? idade;
+    private bool? vivo;
+    private string? medo;
+
+    public string? Nome
+    {
+        get { return nome;}
+        set { nome = value;}
+    }
+    public int? Idade
+    {
+        get {return idade;}
+        set
+        {
+            if (value >=0)
+                idade = value;
+ 
+        }
+    }
+    public bool? Vivo;
+    public string? Medo
+    {
+        get { return medo;}
+        set { medo = value;}   
+    }
+
+    
 }
 
-public class Polemom 
+public class Pokemon 
 {
-    private string? Nome;
-    private string? Tipo;
-    private int? ;
-}
+    private string? nome;
+    private string? tipo;
+    private int? num_pokedex;
+
+    private string? Nome
+    {
+        get { return nome;}
+        set { nome = value;}
+    }    
+    private string? Tipo
+    {
+        get { return tipo;}
+        set { tipo = value;}
+    }
+    private int? NumPokedex;
+}    
