0631f07a8058f10b764eb7292ae338da695119dc
Davi MB <bissondavi@gmail.com>
2026-09-09 13:08:47 -0300

    Create Pokemon class with properties

---
+++ classe_Pokemon.cs
@@ -0,0 +1,18 @@
+public class Pokemon 
+{
+    private string? nome;
+    private string? tipo;
+    private int? num_pokedex;
+
+    private string? Nome
+    {
+        get { return nome;}
+        set { nome = value;}
+    }    
+    private string? Tipo
+    {
+        get { return tipo;}
+        set { tipo = value;}
+    }
+    private int? NumPokedex;
+}    
